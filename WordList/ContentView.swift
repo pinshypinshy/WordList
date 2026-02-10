101b616078dbbd090cfe700544e1124c357843b2
pinshypinshy <hrisyklit@gmail.com>
2026-02-10 13:58:44 +0900

    Initial Commit

---
+++ WordList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordList
+//
+//  Created by 平石悠生 on 2026/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordList/WordListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordListApp.swift
+//  WordList
+//
+//  Created by 平石悠生 on 2026/02/10.
+//
+
+import SwiftUI
+
+@main
+struct WordListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
